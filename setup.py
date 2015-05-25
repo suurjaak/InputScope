143e881b40fe7295ec668baea05ff689770b13d0
Erki Suurjaak <erki@lap.ee>
2015-05-25 18:03:48 +0300

    Updated formatting for PyPI.

--- setup.py
+++ setup.py
@@ -51,6 +51,7 @@ setuptools.setup(
 """Mouse and keyboard input heatmap visualizer and statistics.
 
 Three components:
+
 - main - wxPython desktop tray program, runs listener and webui
 - listener - logs mouse and keyboard input
 - webui - web frontend for statistics and heatmaps
