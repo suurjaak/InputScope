23d8df2d7bde78b15e6fc018a3bafcf2088bce1c
Erki Suurjaak <erki@lap.ee>
2022-01-22 23:18:53 +0200

    v1.5 checkpoint.

--- inputscope/conf.py
+++ inputscope/conf.py
@@ -20,7 +20,7 @@ the declared ones in source code. File is deleted if all values are at default.
 
 @author      Erki Suurjaak
 @created     26.03.2015
-@modified    21.10.2021
+@modified    22.01.2022
 ------------------------------------------------------------------------------
 """
 try: import ConfigParser as configparser # Py2
@@ -35,8 +35,8 @@ import sys
 
 """Program title, version number and version date."""
 Title = "InputScope"
-Version = "1.5.dev2"
-VersionDate = "21.10.2021"
+Version = "1.5"
+VersionDate = "22.01.2022"
 
 """TCP port of the web user interface."""
 WebHost = "localhost"
