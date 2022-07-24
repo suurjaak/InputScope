443ddc7ba78264542d963656c3cdc53629239a94
Erki Suurjaak <erki@lap.ee>
2022-07-24 14:45:01 +0300

    v1.7 checkpoint.

--- src/inputscope/conf.py
+++ src/inputscope/conf.py
@@ -36,7 +36,7 @@ import sys
 
 """Program title, version number and version date."""
 Title = "InputScope"
-Version = "1.7.dev3"
+Version = "1.7"
 VersionDate = "24.07.2022"
 
 """TCP port of the web user interface."""
